955342f3a06aef6990f9011cd7af65c153cc415e
Kian Chen <chinesechenqa@163.com>
2024-12-14 12:53:52 +0800

    use Future to load and display in screen

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:world_time/services/world_time.dart';
 
 class Loading extends StatefulWidget {
   const Loading({super.key});
@@ -8,10 +9,39 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
+
+  late Future<String> _futureTime;
+
+  Future<String> _setupWorldTime() async {
+    WorldTime instance = WorldTime(location: 'Shanghai', flag: 'china.png', url: 'Asia/Shanghai');
+    await instance.getTime();
+    return instance.time ?? 'loading';
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _futureTime = _setupWorldTime();  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Text('loding creen'),
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(50.0),
+        child: FutureBuilder<String>(
+          future: _futureTime,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Text('loading');
+            } else if (snapshot.hasError) {
+              return Text('Error: ${snapshot.error}');
+            } else {
+              return Text(snapshot.data ?? 'loading');
+            }
+          },
+        ),
+      ),
     );
   }
 }
+
